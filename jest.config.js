fb35b2c2fbcfdecdad4d4beba0c7d17c777bdf02
Laxmikant <laxmikantm@smartek21.com>
2020-05-25 13:09:47 +0530

    added jest coverage config

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const {defaults} = require('jest-config');
+module.exports = {
+    verbose:true,
+    testEnvironment: "node",
+    moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+    collectCoverage: true,
+    coverageDirectory :"./coverage_report",
+    collectCoverageFrom:[
+        "exercises/**/*.{js,jsx}",
+        "!**/package.json",
+        "!**/package-lock.json",
+        "!**/node_modules/**",
+        "!**/coverage/**"
+    ],
+    coverageReporters :["text","clover"] //html
+  };
